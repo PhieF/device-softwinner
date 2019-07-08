2f1472ff2c6d25a98d9c35c4e8b8ff088c2a074f
Phie <phie@phie.ovh>
2019-07-08 10:21:35 +0200

    adding cm.mk

---
+++ tulip-chiphd/cm.mk
@@ -0,0 +1,10 @@
+PRODUCT_RELEASE_NAME := pinebook
+
+$(call inherit-product, device/softwinner/tulip-chiphd/tulip_chiphd_pinebook.mk)
+#$(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
+
+PRODUCT_BRAND := Allwinner
+PRODUCT_NAME := tulip_chiphd_pinebook
+PRODUCT_DEVICE := tulip-chiphd
+PRODUCT_MODEL := Pinebook
+PRODUCT_MANUFACTURER := Allwinner
