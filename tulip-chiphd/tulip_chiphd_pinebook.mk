68d363f2b9e079baf1c5d3ac0e4cdde1d9fde282
AlexRouxDanzi <alexandre.roux.danzi@lostpod.me>
2019-09-17 10:39:49 +0200

    clean + use cm vendor tablet

--- tulip-chiphd/tulip_chiphd_pinebook.mk
+++ tulip-chiphd/tulip_chiphd_pinebook.mk
@@ -1,16 +1,6 @@
 $(call inherit-product, device/softwinner/tulip-chiphd/device.mk)
 $(call inherit-product, build/target/product/full_base.mk)
 
-GAPPS_VARIANT := micro
-GAPPS_FORCE_PACKAGE_OVERRIDES := true
-GAPPS_FORCE_BROWSER_OVERRIDES := true
-WITHOUT_CHECK_API = true
-PRODUCT_PACKAGES += \
-    Launcher3
-
-PRODUCT_PACKAGES += \
-    VideoPlayer
-
 PRODUCT_COPY_FILES += \
     device/softwinner/common/config/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
 
@@ -23,8 +13,6 @@ PRODUCT_COPY_FILES += \
 
 # Radio Packages and Configuration Flie
 $(call inherit-product, device/softwinner/common/rild/radio_common.mk)
-#$(call inherit-product, device/softwinner/common/ril_modem/huawei/mu509/huawei_mu509.mk)
-#$(call inherit-product, device/softwinner/common/ril_modem/Oviphone/em55/oviphone_em55.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.sf.lcd_density=160
@@ -37,11 +25,9 @@ PRODUCT_CHARACTERISTICS := tablet
 PRODUCT_AAPT_CONFIG := mdpi large xlarge
 PRODUCT_AAPT_PREF_CONFIG := mdpi
 
-#$(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
-
 PRODUCT_BRAND := Allwinner
 PRODUCT_NAME := tulip_chiphd_pinebook
 PRODUCT_DEVICE := tulip-chiphd
 PRODUCT_MODEL := Pinebook
 PRODUCT_MANUFACTURER := Allwinner
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
